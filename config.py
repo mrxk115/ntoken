b8a0bb1cb86ed2dd6175585cc26ac2e7f8b2a932
mrxk115 <mrxk115@gmail.com>
2025-03-24 16:40:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -40,7 +40,7 @@ TUT_VID = os.environ.get("TUT_VID","https://t.me/Backbencherskabackupchannel/101
 
 #force sub channel id, if you want enable force sub
 FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001911546743"))
-FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002418806520"))
+FORCE_SUB_CHANNEL2 = int(os.environ.get("FORCE_SUB_CHANNEL", "-1002327738776"))
 
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
